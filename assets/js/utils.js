ac1681495027297ff971f2dc454a7ce9d9e6e338
ApichayaWeb <somokhawyai@gmail.com>
2025-09-27 22:54:43 +0700

    Update utils.js

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -6,7 +6,7 @@
 // Global Constants
 const CONFIG = {
     // Google Apps Script Web App URL - ต้องอัปเดตเป็น URL ที่ Deploy จริง
-    API_BASE_URL: 'https://script.google.com/macros/s/AKfycbzI58nRMUVU6jH_hKzWLrnhlbuLaZxWzCo6FqlD0LDRkhl3xOrB9ABu9BgClRvxitc2/exec',
+    API_BASE_URL: 'https://script.google.com/macros/s/AKfycbx9BvTEQ9bLnXCLCaHeWU8x6rzRBCkD-arcVekqPW1SIyB2lshyKx0bNsh3znf9tzf7/exec',
     
     // QR Code patterns
     QR_CODE_PATTERN: /^(\d{2})-(\d{13})$/,
